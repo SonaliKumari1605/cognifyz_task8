6bcd470a03abaccfd195818c7c6eafc05d6694c1
SonaliKumari1605 <160021428+SonaliKumari1605@users.noreply.github.com>
2024-06-21 11:20:42 +0530

    Add files via upload

---
+++ sript.js
@@ -0,0 +1,20 @@
+let blue=document.getElementById("blue");
+let green=document.getElementById("green");
+let gray=document.getElementById("gray");
+let pink=document.getElementById("pink");
+function changecolor(color){
+    document.body.style.background=color;
+}
+function bluecolor(){
+    changecolor('lightblue');
+} 
+function greencolor(){
+    changecolor('lightgreen');
+} 
+function graycolor(){
+    changecolor('silver');
+} 
+function pinkcolor(){
+    changecolor('lightpink');
+} 
+ 
